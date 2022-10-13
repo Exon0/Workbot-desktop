7f4b09de7d84f6e3b5dd2b3ec83a9854ae85a29a
mohsenFennira <mohsen.fennira@esprit.tn>
2022-10-13 12:38:36 +0100

    Add user societe candidat entity

---
+++ src/workbot_jobtn/entites/Candidat.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package workbot_jobtn.entites;
+
+/**
+ *
+ * @author fnmoh
+ */
+public class Candidat {
+    private int id;
+    private String diplome;
+    private String experience;
+    private String niveauFr;
+    private String niveauAng;
+    private String competance;
+    private String cv;
+    private String portfolio;
+    private String bio;
+    private String typeCandidat;
+    private String note;
+
+    public String getDiplome() {
+        return diplome;
+    }
+
+    public String getExperience() {
+        return experience;
+    }
+
+    public String getNiveauFr() {
+        return niveauFr;
+    }
+
+    public String getNiveauAng() {
+        return niveauAng;
+    }
+
+    public String getCompetance() {
+        return competance;
+    }
+
+    public String getCv() {
+        return cv;
+    }
+
+    public String getPortfolio() {
+        return portfolio;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public String getTypeCandidat() {
+        return typeCandidat;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Candidat(int id, String diplome, String experience, String niveauFr, String niveauAng, String competance, String cv, String portfolio, String bio, String typeCandidat, String note) {
+        this.id = id;
+        this.diplome = diplome;
+        this.experience = experience;
+        this.niveauFr = niveauFr;
+        this.niveauAng = niveauAng;
+        this.competance = competance;
+        this.cv = cv;
+        this.portfolio = portfolio;
+        this.bio = bio;
+        this.typeCandidat = typeCandidat;
+        this.note = note;
+    }
+
+    public Candidat(String diplome, String experience, String niveauFr, String niveauAng, String competance, String cv, String portfolio, String bio, String typeCandidat, String note) {
+        this.diplome = diplome;
+        this.experience = experience;
+        this.niveauFr = niveauFr;
+        this.niveauAng = niveauAng;
+        this.competance = competance;
+        this.cv = cv;
+        this.portfolio = portfolio;
+        this.bio = bio;
+        this.typeCandidat = typeCandidat;
+        this.note = note;
+    }
+
+    public Candidat() {
+    } 
+}
